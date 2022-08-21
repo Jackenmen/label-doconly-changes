e6fb52d1c3d1b0dfc741277231bf73f6294e4ccc
Jakub Kuczys <me@jacken.men>
2022-08-22 00:58:19 +0200

    Add basic tests for functions and classes

--- tests/data/hooks/python/is_doc_only_false.py
+++ tests/data/hooks/python/is_doc_only_false.py
@@ -23,3 +23,79 @@ x = 1
 """changed docstring"""
 x = 2
 # ===
+
+
+# [FUNCTIONS]
+# === modified return value ===
+# --- BEFORE ---
+def func(x, y):
+    return x + y
+# --- AFTER ----
+def func(x, y):
+    return x - y
+# ===
+
+# --- BEFORE ---
+def func(x, y):
+    """function docstring"""
+    return x + y
+# --- AFTER ----
+def func(x, y):
+    """function docstring"""
+    return x - y
+# ===
+
+
+# === modified docstring *and* return value ===
+# --- BEFORE ---
+def func(x, y):
+    """function docstring"""
+    return x + y
+# --- AFTER ----
+def func(x, y):
+    """changed docstring"""
+    return x - y
+# ===
+
+
+# [CLASSES]
+# === modified attribute value ===
+# --- BEFORE ---
+class Foo:
+    BAR = "BAZ"
+# --- AFTER ----
+class Foo:
+    BAR = "FOO"
+# --- BEFORE ---
+class Foo:
+    BAR = "BAZ"
+# --- AFTER ----
+class Foo:
+    BAR = "FOO"
+# ===
+
+# --- BEFORE ---
+class Foo:
+    """class docstring"""
+
+    BAR = "BAZ"
+# --- AFTER ----
+class Foo:
+    """class docstring"""
+
+    BAR = "FOO"
+# ===
+
+
+# === modified docstring *and* attribute value ===
+# --- BEFORE ---
+class Foo:
+    """class docstring"""
+
+    BAR = "BAZ"
+# --- AFTER ----
+class Foo:
+    """changed docstring"""
+
+    BAR = "FOO"
+# ===
--- tests/data/hooks/python/is_doc_only_true.py
+++ tests/data/hooks/python/is_doc_only_true.py
@@ -32,3 +32,97 @@ x = 1
 # --- AFTER ----
 x = 1
 # ===
+
+
+# [FUNCTIONS]
+# === identical contents ===
+# --- BEFORE ---
+def func(x, y):
+    return x + y
+# --- AFTER ----
+def func(x, y):
+    return x + y
+# ===
+
+# --- BEFORE ---
+def func(x, y):
+    """function docstring"""
+    return x + y
+# --- AFTER ----
+def func(x, y):
+    """function docstring"""
+    return x + y
+# ===
+
+
+# === modified docstring ===
+# --- BEFORE ---
+def func(x, y):
+    """function docstring"""
+    return x + y
+# --- AFTER ----
+def func(x, y):
+    """changed docstring"""
+    return x + y
+# ===
+
+
+# === removal of a docstring ===
+# --- BEFORE ---
+def func(x, y):
+    """function docstring"""
+    return x + y
+# --- AFTER ----
+def func(x, y):
+    return x + y
+# ===
+
+
+# [CLASSES]
+# === identical contents ===
+# --- BEFORE ---
+class Foo:
+    BAR = "BAR"
+# --- AFTER ----
+class Foo:
+    BAR = "BAR"
+# ===
+
+# --- BEFORE ---
+class Foo:
+    """class docstring"""
+
+    BAR = "BAR"
+# --- AFTER ----
+class Foo:
+    """class docstring"""
+
+    BAR = "BAR"
+# ===
+
+
+# === modified docstring ===
+# --- BEFORE ---
+class Foo:
+    """class docstring"""
+
+    BAR = "BAR"
+# --- AFTER ----
+class Foo:
+    """changed docstring"""
+
+    BAR = "BAR"
+# ===
+
+
+# === removal of a docstring ===
+# --- BEFORE ---
+class Foo:
+    """class docstring"""
+
+    BAR = "BAR"
+# --- AFTER ----
+class Foo:
+
+    BAR = "BAR"
+# ===
